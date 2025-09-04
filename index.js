1e24afc5ba91ea2c08e275c483b5ed5f02586d5f
LochanaNK <lochanakariyawasam10@gmail.com>
2025-09-04 20:52:22 +0530

    canvas setup

---
+++ index.js
@@ -0,0 +1,2 @@
+import {canvas,ctx} from './src/canvas.js';
+
---
+++ src/canvas.js
@@ -0,0 +1,8 @@
+export const canvas = document.querySelector('canvas');
+export const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0,0,canvas.width,canvas.height);
